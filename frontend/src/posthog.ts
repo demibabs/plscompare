3d01bff3dd3c5558f5d64e6f377d9ae232fe8970
demibabs <demibabs@gmail.com>
2026-08-07 11:58:32 -0400

    Set up reverse proxy for better posthog analytics

--- frontend/src/posthog.ts
+++ frontend/src/posthog.ts
@@ -5,7 +5,8 @@ const host = import.meta.env.VITE_PUBLIC_POSTHOG_HOST as string | undefined;
 
 if (key) {
   posthog.init(key, {
-    api_host: host ?? "https://us.i.posthog.com",
+    api_host: 'https://crash.plscompare.com',
+    ui_host: host ?? "https://us.posthog.com",
     defaults: "2026-05-30",
     capture_pageview: "history_change",
   });
